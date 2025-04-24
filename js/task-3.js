d52fa67b59355ae8fb30753b60ba2b4ba7d742df
Vadim Klus <vadim178963@gmail.com>
2025-04-24 22:37:42 +0300

    addtask

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-
-  const inputEl = document.querySelector("#name-input");
-  const outputEl = document.querySelector("#name-output");
-
-  inputEl.addEventListener("input", () => {
-    const trimValue = inputEl.value.trim();
-    outputEl.textContent = trimValue === "" ? "Anonymous" : trimValue;
-  });
